9400ba26549e6957afccafeee32ee113deb3cd88
Warda Benattia <wardaabenattia@gmail.com>
2026-03-21 12:42:42 +0100

    Mission 6 T1 : tests unitaires Service et Utilisateur

---
+++ MediaTekDocumentsTests/ServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MediaTekDocuments.model;
+
+namespace MediaTekDocumentsTests
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        private const string id = "ADM";
+        private const string libelle = "administratif";
+        private readonly Service service = new Service(id, libelle);
+
+        [TestMethod]
+        public void ServiceTest()
+        {
+            Assert.AreEqual(id, service.Id, "devrait réussir : id valorisé");
+            Assert.AreEqual(libelle, service.Libelle, "devrait réussir : libellé valorisé");
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            Assert.AreEqual(libelle, service.ToString(), "devrait réussir : libellé retourné");
+        }
+    }
+}
---
+++ MediaTekDocumentsTests/UtilisateurTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MediaTekDocuments.model;
+
+namespace MediaTekDocumentsTests
+{
+    [TestClass]
+    public class UtilisateurTests
+    {
+        private const int id = 1;
+        private const string login = "admin";
+        private const string pwd = "admin";
+        private const string idService = "ADM";
+        private const string libelleService = "administratif";
+        private static readonly Utilisateur utilisateur = new Utilisateur(id, login, pwd, idService, libelleService);
+
+        [TestMethod]
+        public void UtilisateurTest()
+        {
+            Assert.AreEqual(id, utilisateur.Id, "devrait réussir : id valorisé");
+            Assert.AreEqual(login, utilisateur.Login, "devrait réussir : login valorisé");
+            Assert.AreEqual(pwd, utilisateur.Pwd, "devrait réussir : pwd valorisé");
+            Assert.AreEqual(idService, utilisateur.IdService, "devrait réussir : idService valorisé");
+            Assert.AreEqual(libelleService, utilisateur.LibelleService, "devrait réussir : libelleService valorisé");
+            Assert.AreEqual(idService, utilisateur.Service.Id, "devrait réussir : service.Id valorisé");
+        }
+    }
+}
